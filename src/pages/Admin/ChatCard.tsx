d5d08995002a384b39d10825f54d318be224cfd8
Rohanmi97 <rohan.openforwork@gmail.com>
2024-08-26 10:32:41 +0530

    message box activated

--- src/pages/Admin/ChatCard.tsx
+++ src/pages/Admin/ChatCard.tsx
@@ -1,3 +1,6 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 import { Chat } from '../../types/chat';
 import UserOne from '../../images/user/user-01.png';
@@ -56,61 +59,6 @@ const chatData: Chat[] = [
     color: '#FFBA00',
   },
 ];
-
-// const ChatCard = () => {
-//   return (
-//     <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
-//       <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
-//         Chats
-//       </h4>
-
-//       <div>
-//         {chatData.map((chat, key) => (
-//           <Link
-//             to="/"
-//             className="flex items-center gap-5 py-3 px-7.5 hover:bg-gray-3 dark:hover:bg-meta-4"
-//             key={key}
-//           >
-//             <div className="relative h-14 w-14 rounded-full">
-//               <img src={chat.avatar} alt="User" />
-//               <span
-//                 className="absolute right-0 bottom-0 h-3.5 w-3.5 rounded-full border-2 border-white"
-//                 style={{backgroundColor: chat.color}}
-//               ></span>
-//             </div>
-
-//             <div className="flex flex-1 items-center justify-between">
-//               <div>
-//                 <h5 className="font-medium text-black dark:text-white">
-//                   {chat.name}
-//                 </h5>
-//                 <p>
-//                   <span className="text-sm text-black dark:text-white">
-//                     {chat.text}
-//                   </span>
-//                   <span className="text-xs"> . {chat.time} min</span>
-//                 </p>
-//               </div>
-//               {chat.textCount !== 0 && (
-//                 <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
-//                   <span className="text-sm font-medium text-white">
-//                     {' '}
-//                     {chat.textCount}
-//                   </span>
-//                 </div>
-//               )}
-//             </div>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatCard;
-
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const ChatCard = () => {
   const [activeTab, setActiveTab] = useState('query');
@@ -183,7 +131,7 @@ const ChatCard = () => {
             {chatData.map((chat, key) => (
               <div key={key}>
                 <Link
-                  to="/"
+                  to="/admin/test"
                   className="flex items-center gap-5 py-3 px-7.5 hover:bg-gray-3 dark:hover:bg-meta-4"
                 >
                   <div className="flex flex-1 items-center justify-between">
